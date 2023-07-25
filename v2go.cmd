98ede634ae795e6cd9e7224a34d72aecc6c81b0e
NY5667 <82657677+NY5667@users.noreply.github.com>
2023-07-25 16:45:07 +0800

    update

--- v2go.cmd
+++ v2go.cmd
@@ -4,7 +4,7 @@ start "" "%~dp0v2free\v2free.exe"  -config v2free\v2free.json
 
 %SystemRoot%\System32\reg.exe query "HKLM\Software\Microsoft\Windows\CurrentVersion\App Paths\chrome.exe" >nul 2>&1
 IF  not errorlevel 1 (
-start chrome.exe --user-data-dir=%~dp0chrome-user-data  --proxy-server="socks5://127.0.0.1:20808" --host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE 127.0.0.1"  https://www.bannedbook.org/bnews/fq/?utm_source=v2free-win
+start chrome.exe --user-data-dir=%~dp0chrome-user-data  --proxy-server="socks5://127.0.0.1:20808" --host-resolver-rules="MAP * ~NOTFOUND , EXCLUDE 127.0.0.1"
 ) else (
 	echo Chrome浏览器不存在或没有正确安装，请尝试重新安装Chrome浏览器
 )
